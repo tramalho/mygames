7ee49a0eceeef1d93324fb3b3504b96538d17859
tramalho <thiagofacul@gmail.com>
2020-09-06 12:14:20 -0300

    108. Entendendo Core Data

---
+++ MyGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGames
+//
+//  Created by Thiago Antonio Ramalho on 06/09/20.
+//  Copyright © 2020 Tramalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
