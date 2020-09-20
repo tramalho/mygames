2bf49eeb6958b78940a9cce4c36f1693cab4d08b
tramalho <thiagofacul@gmail.com>
2020-09-20 12:46:45 -0300

    115. Atribuindo fotos aos nossos jogos

--- MyGames/GameAddEditViewController.swift
+++ MyGames/GameAddEditViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 class GameAddEditViewController: UIViewController {
-
+    
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var platform: UITextField!
     @IBOutlet weak var releaseDate: UIDatePicker!
     @IBOutlet weak var cover: UIImageView!
+    @IBOutlet weak var coverButton: UIButton!
     
     private var game: Game!
     private var platformManager = PlatformManager.shared
@@ -21,13 +22,43 @@ class GameAddEditViewController: UIViewController {
         let pickerView = UIPickerView()
         pickerView.dataSource = self
         pickerView.delegate = self
-    
+        
         return pickerView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupPickerView()
+    }
+    
+    private func setupPickerView() {
+        pickerView.backgroundColor = .white
+        let canncelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        let flexButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 44))
+        toolbar.tintColor = UIColor(named: "primary")
+        toolbar.items = [canncelButton, flexButton, doneButton]
+        
         platform.inputView = pickerView
+        
+        platform.inputAccessoryView = toolbar
+    }
+    
+    @objc private func cancel() {
+        platform.resignFirstResponder()
+    }
+    
+    @objc private func done() {
+        if let platform = getPlatformFromPickerView() {
+            self.platform?.text = platform.name
+        }
+        cancel()
+    }
+    
+    private func getPlatformFromPickerView() -> Platform? {
+        return platformManager.platforms[pickerView.selectedRow(inComponent: 0)]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +66,18 @@ class GameAddEditViewController: UIViewController {
     }
     
     @IBAction func addNewCover(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Selecionar Image", message: "De onde você deseja obter a imagem?", preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            alert.addAction(createActionSheet(sourceType: .camera, title: "Câmera"))
+        }
+        
+        alert.addAction(createActionSheet(sourceType: .photoLibrary, title: "Biblioteca de Fotos"))
+        alert.addAction(createActionSheet(sourceType: .savedPhotosAlbum, title: "Álbum de Fotos"))
+        
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func save(_ sender: UIButton) {
@@ -46,6 +89,12 @@ class GameAddEditViewController: UIViewController {
         game.name = name.text
         game.releaseDate = releaseDate.date
         
+        if !(platform.text?.isEmpty ?? true) {
+            game.platform = getPlatformFromPickerView()
+        }
+        
+        game.cover = cover.image
+        
         do {
             try context.save()
         } catch  {
@@ -53,6 +102,20 @@ class GameAddEditViewController: UIViewController {
         }
         
         navigationController?.popViewController(animated: true)
+    }
+    
+    private func createActionSheet(sourceType: UIImagePickerController.SourceType, title: String) -> UIAlertAction {
+        return UIAlertAction(title: title, style: .default, handler: { (UIAlertAction) in
+            self.selectPicture(sourceType: sourceType)
+        })
+    }
+    
+    private func selectPicture(sourceType: UIImagePickerController.SourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = self
+        imagePicker.navigationBar.tintColor = UIColor(named: "primary")
+        present(imagePicker, animated: true, completion: nil)
     }
 }
 
@@ -67,5 +130,16 @@ extension GameAddEditViewController: UIPickerViewDelegate, UIPickerViewDataSourc
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return platformManager.platforms[row].name
+    }
+}
+
+extension GameAddEditViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            cover.image = image
+            coverButton.setTitle(nil, for: .normal)
+            dismiss(animated: true, completion: nil)
+        }
     }
 }
